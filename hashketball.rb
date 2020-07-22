cdb7c463b5dd3169c0ad1820a447193bf93a8d88
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 01:10:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,28 +69,28 @@ def game_hash
   
   
   the_hash[:away] = {}
-  the_hash[:home][:team_name] = "Brooklan Nets"
-  the_hash[:home][:colors] = ["Black", "White"]
+  the_hash[:away][:team_name] = "Brooklan Nets"
+  the_hash[:away][:colors] = ["Black", "White"]
   
-  the_hash[:home][:players] = []
+  the_hash[:away][:players] = []
   
-  the_hash[:home][:players][0] = {}
-  the_hash[:home][:players][1] = {}
-  the_hash[:home][:players][2] = {}
-  the_hash[:home][:players][3] = {}
-  the_hash[:home][:players][4] = {}
+  the_hash[:away][:players][0] = {}
+  the_hash[:away][:players][1] = {}
+  the_hash[:away][:players][2] = {}
+  the_hash[:away][:players][3] = {}
+  the_hash[:away][:players][4] = {}
   
-  the_hash[:home][:players][0][:player_name] = "Alan Anderson"
-  the_hash[:home][:players][1][:player_name] = "Reggie Evans"
-  the_hash[:home][:players][2][:player_name] = "Brook Lopez"
-  the_hash[:home][:players][3][:player_name] = "Mason Plumlee"
-  the_hash[:home][:players][4][:player_name] = "Jason Terry"
+  the_hash[:away][:players][0][:player_name] = "Alan Anderson"
+  the_hash[:away][:players][1][:player_name] = "Reggie Evans"
+  the_hash[:away][:players][2][:player_name] = "Brook Lopez"
+  the_hash[:away][:players][3][:player_name] = "Mason Plumlee"
+  the_hash[:away][:players][4][:player_name] = "Jason Terry"
   
-  the_hash[:home][:players][0][:number] = 0
-  the_hash[:home][:players][1][:number] = 30
-  the_hash[:home][:players][2][:number] = 11
-  the_hash[:home][:players][3][:number] = 1
-  the_hash[:home][:players][4][:number] = 31
+  the_hash[:away][:players][0][:number] = 0
+  the_hash[:away][:players][1][:number] = 30
+  the_hash[:away][:players][2][:number] = 11
+  the_hash[:away][:players][3][:number] = 1
+  the_hash[:away][:players][4][:number] = 31
   
   the_hash[:home][:players][0][:shoe] = 16
   the_hash[:home][:players][1][:shoe] = 14
