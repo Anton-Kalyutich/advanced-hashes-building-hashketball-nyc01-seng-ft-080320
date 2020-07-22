c798eb16a44aee8658423ba660d7e7f6a0f178f8
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 00:46:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,10 @@
 def game_hash
   the_hash = {}
+  
   the_hash[:home] = {}
-  the_hash[:away] = {}
   the_hash[:home][:team_name] = "Brooklan Nets"
   the_hash[:home][:colors] = ["Black", "White"]
+  
+  the_hash[:away] = {}
   the_hash
 end
