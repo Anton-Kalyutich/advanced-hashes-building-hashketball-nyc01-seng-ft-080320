2fcc3578e4aa811b5add1414761820c4d370374c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 01:07:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,17 +19,17 @@ def game_hash
   the_hash[:home][:players][3][:player_name] = "Mason Plumlee"
   the_hash[:home][:players][4][:player_name] = "Jason Terry"
   
-  the_hash[:home][:players][0][:number] = "0"
-  the_hash[:home][:players][1][:number] = "30"
-  the_hash[:home][:players][2][:number] = "11"
-  the_hash[:home][:players][3][:number] = "1"
-  the_hash[:home][:players][4][:number] = "31"
+  the_hash[:home][:players][0][:number] = 0
+  the_hash[:home][:players][1][:number] = 30
+  the_hash[:home][:players][2][:number] = 11
+  the_hash[:home][:players][3][:number] = 1
+  the_hash[:home][:players][4][:number] = 31
   
-  the_hash[:home][:players][0][:shoe] = "16"
-  the_hash[:home][:players][1][:shoe] = "14"
-  the_hash[:home][:players][2][:shoe] = "17"
-  the_hash[:home][:players][3][:shoe] = "19"
-  the_hash[:home][:players][4][:shoe] = "15"
+  the_hash[:home][:players][0][:shoe] = 16
+  the_hash[:home][:players][1][:shoe] = 14
+  the_hash[:home][:players][2][:shoe] = 17
+  the_hash[:home][:players][3][:shoe] = 19
+  the_hash[:home][:players][4][:shoe] = 15
   
   the_hash[:home][:players][0][:points] = 22
   the_hash[:home][:players][1][:points] = 12
