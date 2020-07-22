bb3a3e76658b342fab00402de5cae652d1bde36c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 00:54:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,14 +11,12 @@ def game_hash
   the_hash[:home][:players][2][:player_name] = "Brook Lopez"
   the_hash[:home][:players][3][:player_name] = "Mason Plumlee"
   the_hash[:home][:players][4][:player_name] = "Jason Terry"
-  the_hash[:home][:players][5][:player_name] = "Alan Anderson"
   
   the_hash[:home][:players][0][:number] = 0
-  the_hash[:home][:players][1][:player_name] = "Reggie Evans"
-  the_hash[:home][:players][2][:player_name] = "Brook Lopez"
-  the_hash[:home][:players][3][:player_name] = "Mason Plumlee"
-  the_hash[:home][:players][4][:player_name] = "Jason Terry"
-  the_hash[:home][:players][5][:player_name] = "Alan Anderson"
+  the_hash[:home][:players][1][:number] = 0
+  the_hash[:home][:players][2][:number] = 0
+  the_hash[:home][:players][3][:number] = 0
+  the_hash[:home][:players][4][:number] = 0
   
   
   
